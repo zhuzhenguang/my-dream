ba96f43a54e52c03ce86b0c00cd519c0d763c838
zhuzhenguang <zhuzhenguang2008@gmail.com>
2019-03-17 00:02:58 +0800

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import 'moment';
+import * as moment from 'moment/';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass',]
+  styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'my-dream-app';
+export class AppComponent implements OnInit {
+  tableData: any[] = [];
+  startDate = '';
+  endDate = '';
+  private originTableData: any[];
+  private defaultStartDate = '1111-01-01';
+  private defaultEndDate = '9999-01-01';
+
+  constructor() {
+    this.onFilter = this.onFilter.bind(this);
+  }
+
+  ngOnInit(): void {
+    this.tableData = [{
+      name: 'Zhenguang Zhu 1',
+      date: '2019-03-19',
+      address: '上海市普陀区金沙江路 1518 弄',
+    }, {
+      name: 'Zhenguang Zhu 2',
+      date: '2019-03-20',
+      address: '上海市普陀区金沙江路 1518 弄',
+    }, {
+      name: 'Ming Jiao 3',
+      date: '2019-03-21',
+      address: '上海市普陀区金沙江路 1518 弄',
+    }, {
+      name: 'Ming Jiao 4',
+      date: '2019-03-22',
+      address: '上海市普陀区金沙江路 1510 弄',
+    }];
+    this.originTableData = this.tableData.slice(0);
+  }
+
+  onFilter(): void {
+    const startDate = this.startDate.trim() || this.defaultStartDate;
+    const endDate = this.endDate.trim() || this.defaultEndDate;
+
+    this.tableData = this.originTableData.filter(data => {
+      return moment(data.date).isBetween(startDate.trim(), endDate.trim());
+    });
+  }
+
+  clearStartDate(): void {
+    this.startDate = '';
+  }
+
+  clearEndDate(): void {
+    this.endDate = '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ElModule} from 'element-angular/release/element-angular.module';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ElModule} from 'element-angular/release/element-angular.module';
 
 @NgModule({
   declarations: [
@@ -12,9 +13,11 @@ import { ElModule} from 'element-angular/release/element-angular.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     ElModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
